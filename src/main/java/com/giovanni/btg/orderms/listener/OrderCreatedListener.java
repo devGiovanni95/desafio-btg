8968008fcc054d1bd4ed9335ab88680e1a030e44
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-11-20 23:41:49 -0300

    Created logger to view messages

--- src/main/java/com/giovanni/btg/orderms/listener/OrderCreatedListener.java
+++ src/main/java/com/giovanni/btg/orderms/listener/OrderCreatedListener.java
@@ -1,6 +1,8 @@
 package com.giovanni.btg.orderms.listener;
 
 import com.giovanni.btg.orderms.listener.dto.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
@@ -10,8 +12,10 @@ import static com.giovanni.btg.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE
 @Component
 public class OrderCreatedListener {
 
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message){
-
+        logger.info("Message consumed: {}", message);
     }
 }
