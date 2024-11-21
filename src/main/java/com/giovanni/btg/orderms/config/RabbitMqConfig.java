403e9ea840f6b5a4ade6bfce3b7bd64c2ce79d75
devGiovanni95 <giovanni.devjava@hotmail.com>
2024-11-20 23:40:46 -0300

    Created config to create automatic files and converter to messages json

--- src/main/java/com/giovanni/btg/orderms/config/RabbitMqConfig.java
+++ src/main/java/com/giovanni/btg/orderms/config/RabbitMqConfig.java
@@ -1,9 +1,25 @@
 package com.giovanni.btg.orderms.config;
 
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
 
     public static final String ORDER_CREATED_QUEUE = "btg-pactual-order-created";
+
+    //Convertendo message que vem do rabbitmq
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    //autoriza a criar a fila
+    @Bean
+    public Declarable orderCreatedQueue(){
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
 }
